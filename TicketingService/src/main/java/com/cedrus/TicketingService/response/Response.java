f2677d10d5205aa8e6f03927ccedb2a266c9aed8
atabball <anthony_tabbal@hotmail.com>
2023-04-03 23:23:55 +0300

    adding Serial annotation

--- TicketingService/src/main/java/com/cedrus/TicketingService/response/Response.java
+++ TicketingService/src/main/java/com/cedrus/TicketingService/response/Response.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @Setter
@@ -13,6 +14,7 @@ import java.io.Serializable;
 @AllArgsConstructor
 public class Response<T> implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
     private String status;
     private String message;
